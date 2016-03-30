900311141fc4c67afc1996561e94e067fa41210e
johnm25 <johnm252001@yahoo.com>
2016-03-30 21:15:51 +0200

    d

--- iTextSharpTest/iTextSharpTest/UnitTest1.cs
+++ iTextSharpTest/iTextSharpTest/UnitTest1.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace iTextSharpTest
+﻿namespace iTextSharpTest
 {
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.IO;
     using Helper;
     using iTextSharp;
